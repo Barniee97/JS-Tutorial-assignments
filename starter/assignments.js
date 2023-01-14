d2830882a13a2ccf818d522719fc25da4837fb44
Arah Barnabas Onuh <arahbarnabasonuh@gmail.com>
2023-01-14 02:30:00 +0100

    Worked on variables

---
+++ starter/assignments.js
@@ -0,0 +1,9 @@
+// Values and Variables 
+
+let country = ("Nigeria");
+let continent = ("Africa");
+let population = (213400000);
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ starter/script.js
@@ -0,0 +1,20 @@
+// Variable
+
+let js = "amazing";
+console.log(4+5+6);
+
+console.log("munir")
+
+
+let firstName = ("Amanda");
+console.log(firstName);
+
+let chi_barnes = ("thingy");
+console.log(chi_barnes);
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Baller";
+
+console.log(myFirstJob);
