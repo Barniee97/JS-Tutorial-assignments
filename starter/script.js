65c15f46ab189c93662da2952a5451687776013f
Arah Barnabas Onuh <arahbarnabasonuh@gmail.com>
2023-01-18 04:43:13 +0100

    modified datatypes

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,12 @@
 // Variable
 
+/*
 let js = "amazing";
 console.log(4+5+6);
 
 console.log("munir")
 
-
+// Variable name conventions
 let firstName = ("Amanda");
 console.log(firstName);
 
@@ -18,3 +19,27 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "Baller";
 
 console.log(myFirstJob);
+*/
+
+// Boolean
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true); 
+console.log(typeof javaScriptIsFun); 
+// console.log(typeof 23); 
+// console.log(typeof "cat"); 
+
+
+javaScriptIsFun = "YES!";
+console.log(javaScriptIsFun);
+
+let school;
+console.log(school);
+console.log(typeof school);
+
+school = 1997;
+console.log(typeof school);
+
