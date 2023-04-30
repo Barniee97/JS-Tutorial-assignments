d33066c5b6e00f732243926ebfa968772a80e88d
Arah Barnabas Onuh <arahbarnabasonuh@gmail.com>
2023-04-30 19:20:31 +0100

    i worked on the check button functionality and msg

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,9 +10,14 @@
 // document.querySelector(".guess").value = 12;
 // console.log(document.querySelector(".guess").value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1; //to generate a secret numbert
+let score = 20; //you set score at 20 i.e fixed score that will change if guesses are wrong
+
 document.querySelector(".number").textContent = secretNumber;
 
+//this method is to take action when the button (checked) is clicked, it takes the input value (guess) and run the if and else statements
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess, typeof guess);
@@ -24,9 +29,22 @@ document.querySelector(".check").addEventListener("click", function() {
         document.querySelector(".message").textContent = "🎊 Correct Number!";
     }
     else if(guess > secretNumber) {
-        document.querySelector(".message").textContent = "📈 Too high!";
+        if(score > 1) {
+            document.querySelector(".message").textContent = "📈 Too high!";
+            //to show reduction in score over a wrong guess
+            score--;
+            document.querySelector(".score").textContent = score;
+         } else {
+            document.querySelector(".message").textContent = "💥 You lost the game!"
+         }
+
+        
     }
     else if(guess < secretNumber) {
         document.querySelector(".message").textContent = "📉 Too low!";
+
+        //to show reduction in score over a wrong guess
+        score--;
+        document.querySelector(".score").textContent = score;
     }
 });
